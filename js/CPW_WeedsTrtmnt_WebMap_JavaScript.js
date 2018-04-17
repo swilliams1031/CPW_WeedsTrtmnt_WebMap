79bda7912a6fffd32e79fddfac4073f01fd7cb3b
Steve Williams <swilliams303@student.cccs.edu>
2018-04-17 11:12:23 -0600

    create basic functionality
    added meta ref to  scale map to allow zooming on map and not page
    
    added meta ref to enable Internet Explorer and Edge compatibility
    
    called the ArcGIS 3.23 Javascript API  and the corresponding CSS stlyesheet
    
    added CSS to style the map view pane
    
    added JS to insert basic map; tried to add feature layer but that failed

--- js/CPW_WeedsTrtmnt_WebMap_JavaScript.js
+++ js/CPW_WeedsTrtmnt_WebMap_JavaScript.js
@@ -4,8 +4,29 @@ document.addEventListener('DOMContentLoaded', function() {
     this is the event callback function that is called when the document content has been loaded
     we need our map container to be ready before dropping our map in, for example
     *this is new as js bin takes care of this for you
-    
+
     Put all of the ArcGIS API stuff in here.  This is what makes the map work.
     */
-    
+
+    require([
+        "esri/map",
+        "esri/layers/FeatureLayer",
+        "dojo/domReady!"
+    ],
+            function(
+            Map,
+             FeatureLayer
+            ) {
+
+        var map = new Map("map", {
+            basemap: "hybrid",
+            center: [-105.7821, 39.5501],
+            zoom: 7
+        });
+
+        var featureLayer = new FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/COParks_WeedTreat_Tracking/FeatureServer");
+
+        map.addLayer(featureLayer);
+
+    });
 });
